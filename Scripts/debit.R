1522ee480f1a2334eaa2f6696dd764c55b6a77f3
blanchette31 <brandonblanchette.30@gmail.com>
2022-09-21 16:30:14 -0400

    changes to files

--- Scripts/debit.R
+++ Scripts/debit.R
@@ -2,7 +2,8 @@
 library(ggplot2)
 library(gridExtra)
 library(dplyr)
-
+library(data.table)
+library(tidyverse)
 
 # Load dataframe
 
@@ -13,6 +14,17 @@ df = read.csv("Data//Raw//debit", pattern = "*.csv", full.names = TRUE, sep = " 
   lapply(read_csv) %>%
   bind_rows
 
-as.data.frame(df)
+as.data.frame(df2003)
 
 write.csv(df, "Data//Processed//debit//debit_merged.csv", row.names = TRUE)
+
+
+#list discharge files
+debit_files = list.files("Data//Raw//debit//", pattern = ".csv", full.names = FALSE)
+ 
+#loop to read csv files
+lapply(debit_files, read_delim
+
+for (i in 1:length(debit_files)){
+  assign(debit_files[i], read.csv(debit_files[i]))
+  }
